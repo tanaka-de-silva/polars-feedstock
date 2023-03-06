5cda42069b9e4472e0475458afb6be03c88968d3
Tanaka de Silva <tanaka@biscuitcrumbs.com>
2023-03-06 11:47:49 +0000

    Compile linux_aarch64

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,13 @@ if [[ "${target_platform}" == "linux-64" ]]; then
   export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+avx,+fma'
 fi
 
+if [[ "${target_platform}" == "linux-aarch64" ]]; then
+  # similar to settings upstream in polars
+  export JEMALLOC_SYS_WITH_LG_PAGE="16"
+  # attempt to reduce memory usage
+  export CARGO_BUILD_JOBS="1"
+fi
+
 # Run the maturin build via pip which works for direct and
 # cross-compiled builds.
 $PYTHON -m pip install . -vv
